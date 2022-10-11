63079d128715e50d14239f88c94218f2f182ce3f
lifework <kobayashi@lifework.tech>
2022-10-11 14:45:29 +0900

    プロジェクト作成

---
+++ WPF_UserPicture/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using System.Windows;
+using WPF_UserPicture.Views;
+
+namespace WPF_UserPicture
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
