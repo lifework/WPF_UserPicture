9b5a0effb9bbd120c14e7e21509fbc11af9b0140
lifework <kobayashi@lifework.tech>
2022-10-11 23:46:21 +0900

    GetPictureAsync して、BitmapImage で表示する

--- WPF_UserPicture/ViewModels/MainWindowViewModel.cs
+++ WPF_UserPicture/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,11 @@
-﻿using Prism.Mvvm;
+﻿using ImTools;
+using Prism.Mvvm;
 using Reactive.Bindings;
 using System;
+using System.IO;
 using System.Linq;
 using System.Windows.Media.Imaging;
+using Windows.Networking.NetworkOperators;
 using Windows.Storage.Streams;
 using Windows.System;
 
@@ -26,7 +29,22 @@ namespace WPF_UserPicture.ViewModels
 
             AccountName.Value = await user.GetPropertyAsync(KnownUserProperties.AccountName) as string;
 
+
+            // C# converting UWP BitmapImage to WPF BitmapImage
+            // https://stackoverflow.com/questions/50596735/c-sharp-converting-uwp-bitmapimage-to-wpf-bitmapimage
             IRandomAccessStreamReference streamReference = await user.GetPictureAsync(UserPictureSize.Size208x208);
+            if (streamReference != null)
+            {
+                var bitmap = new BitmapImage();
+                using var randomAccessStream = await streamReference.OpenReadAsync();
+                using var stream = randomAccessStream.AsStream();
+
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.StreamSource = stream;
+                bitmap.EndInit();
+                UserPicture.Value = bitmap;
+            }
         }
     }
 }
